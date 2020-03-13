fab2f93b6a0edf053644954f22c21c0c5ce5e2a8
Devin Stagg <devinstagg7@gmail.com>
2020-03-12 20:22:05 -0600

    git add . git commit .

--- apitest/index.js
+++ apitest/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 4000
+var colors = require('colors');
+
 
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`.rainbow))
