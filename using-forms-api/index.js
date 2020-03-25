1eeb0f9d7b358d9a229108681336b9d2d942d482
Devin Stagg <devinstagg7@gmail.com>
2020-03-24 20:01:08 -0600

    git add . git commit . git push .

--- using-forms-api/index.js
+++ using-forms-api/index.js
@@ -2,7 +2,27 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const users = require('./users.js')
 
 app.post('/users', (req, res) => {
+    res.send('A new user has been added' )
+})
+
+app.get('/users/:id', (req, res) => {
+    if(user_id === user_id) {
+        res.send('User exists in database')
+    } else {
+        res.send('This user does NOT exist')
+    }
     
 })
+
+
+app.patch('/users/:id', (req, res) => {
+    res.send('Password has been updated')
+})
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ using-forms-api/users.js
@@ -0,0 +1,19 @@
+const customers = [
+    {
+        user_id: 1,
+        first_name: "Devin",
+        last_name: "Stagg",
+        email: "devintest@gmail.com",
+        password: 'test1234'
+    },
+
+    {
+        user_id: 2,
+        first_name: "Ashley",
+        last_name: "Stagg",
+        email: "ashleytest@gmail.com",
+        password: 'test12345678'
+    }
+]
+
+module.export = customers;
